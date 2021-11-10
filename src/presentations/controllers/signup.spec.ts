c8616c793322bd3fb0f6421f6bf3c0330953180e
Welisson luca <welissonluca17@gmail.com>
2021-11-10 13:32:32 -0300

    test: ensure signupController return an error if no email is provided

--- src/presentations/controllers/signup.spec.ts
+++ src/presentations/controllers/signup.spec.ts
@@ -17,4 +17,21 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
+
+  it('should return for handle if no email is provided', () => {
+    const sut = new SignupController();
+
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        password: 'any password',
+        passwordConfirmation: 'any password confirmation',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  });
 });
