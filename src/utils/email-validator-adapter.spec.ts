0f94b00dcab440e29f818161692a1f5bf4a26b0e
Welisson luca <welissonluca17@gmail.com>
2022-01-02 17:39:56 -0300

    test: ensure email validator adapter call validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -25,4 +25,14 @@ describe('EmailValidator Adapter', () => {
 
     expect(isValid).toBe(true);
   });
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+
+    sut.isValid('any_email@mail.com');
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
 });
