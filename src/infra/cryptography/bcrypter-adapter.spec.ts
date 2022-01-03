95bf3df2ba4b119e7bfe3d12005dcd1d21961eeb
Welisson luca <welissonluca17@gmail.com>
2022-01-03 00:06:09 -0300

    test: ensure bcrypt adapter return a hash on success

--- src/infra/cryptography/bcrypter-adapter.spec.ts
+++ src/infra/cryptography/bcrypter-adapter.spec.ts
@@ -2,6 +2,12 @@ import bcrypt from 'bcrypt';
 
 import { BcryptAdapter } from './bcrypter-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve('any_hash'));
+  },
+}));
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const salt = 12;
@@ -12,5 +18,14 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value');
 
     expect(bcrypt.hash).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+
+    const hash = await sut.encrypt('any_value');
+
+    expect(hash).toBe('any_hash');
   });
 });
