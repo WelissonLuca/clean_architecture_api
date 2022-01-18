557d97d8e6886299090eb617bc7a6c8bc953b518
Welisson luca <welissonluca17@gmail.com>
2022-01-17 21:22:39 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/config/middlewares/content-type.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+
+import app from '../app';
+
+describe('Content Type Middleware', () => {
+  test('should return default content-type as json', async () => {
+    app.get('/test-content-type', (req, res) => {
+      res.send();
+    });
+    await request(app).get('/test-content-type').expect('content-type', /json/);
+  });
+
+  test('should return xml content-type when forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml');
+      res.send();
+    });
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/);
+  });
+});
