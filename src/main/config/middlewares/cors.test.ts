6293dd4f7a4106aa6d30c8179ac4c25e4eca2d53
Welisson luca <welissonluca17@gmail.com>
2022-01-17 21:16:51 -0300

    feat: ensure requests enable cors

---
+++ src/main/config/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+
+import app from '../app';
+
+describe('Cors Middleware', () => {
+  test('should enable cors', async () => {
+    app.get('/test-cors', (req, res) => {
+      res.send();
+    });
+    await request(app)
+      .get('/test-cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('acess-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*');
+  });
+});
