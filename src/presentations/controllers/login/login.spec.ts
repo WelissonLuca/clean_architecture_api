95d2ece0af6f37d96014e085b71dce25c0793c7a
Welisson luca <welissonluca17@gmail.com>
2022-02-03 22:08:35 -0300

    feat: ensure login controller return 400 if no password no provider

--- src/presentations/controllers/login/login.spec.ts
+++ src/presentations/controllers/login/login.spec.ts
@@ -15,4 +15,17 @@ describe('Login Controller', () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
   });
+
+  it('Should return 400 if no password is provided', async () => {
+    const sut = new LoginController();
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+  });
 });
