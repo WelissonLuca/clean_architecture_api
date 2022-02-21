d0ff778b4428b9bb4825680d5c312b49d3e3b5f0
Welisson luca <welissonluca17@gmail.com>
2022-02-21 08:29:59 -0300

    feat: ensure login controller calls authentication with correct values

--- src/presentations/controllers/login/login.ts
+++ src/presentations/controllers/login/login.ts
@@ -1,12 +1,16 @@
+import { IAuthentication } from '../../../domain/useCases/authentication';
 import { MissingParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http';
+import { badRequest, ok, serverError } from '../../helpers/http';
 import { IController } from '../../protocols/controller';
 import { IHttpResponse, IHttpRequest } from '../../protocols/http';
 import { IEmailValidator } from '../signup/signup-protocols';
 
 export class LoginController implements IController {
-  constructor(private readonly emailValidator: IEmailValidator) {}
-  handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+  constructor(
+    private readonly emailValidator: IEmailValidator,
+    private readonly authentication: IAuthentication
+  ) {}
+  async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
       const { email, password } = httpRequest.body;
       if (!email) {
@@ -21,7 +25,9 @@ export class LoginController implements IController {
       if (!isValid) {
         return Promise.resolve(badRequest(new MissingParamError('email')));
       }
-      return Promise.resolve(undefined);
+      await this.authentication.auth(email, password);
+
+      return Promise.resolve(ok({}));
     } catch (error) {
       return Promise.resolve(serverError(error));
     }
