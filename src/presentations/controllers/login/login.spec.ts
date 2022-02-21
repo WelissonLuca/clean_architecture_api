48e5a4cd57a8153bf1816f2ae33ff70c2694ae2e
Welisson luca <welissonluca17@gmail.com>
2022-02-21 08:40:58 -0300

    feat: ensure login controller returns 401 if invalid credentials are provided

--- src/presentations/controllers/login/login.spec.ts
+++ src/presentations/controllers/login/login.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable new-cap */
 import { IAuthentication } from '../../../domain/useCases/authentication';
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http';
+import { badRequest, serverError, unauthorized } from '../../helpers/http';
 import { IEmailValidator } from '../../protocols/email-validator';
 import { IHttpRequest } from '../../protocols/http';
 import { LoginController } from './login';
@@ -103,12 +103,23 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(serverError(new Error()));
   });
 
-  it('Should return 400 if no password is provided', async () => {
+  it('Should calls Authentication with correct values', async () => {
     const { sut, authenticationStub } = makeSut();
     const authSpy = jest.spyOn(authenticationStub, 'auth');
 
     await sut.handle(makeFakeRequest());
 
     expect(authSpy).toHaveBeenCalledWith('valid@gmail.com', 'valid_password');
+  });
+
+  it('Should return 400 if invalid credentials are provided', async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest
+      .spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(Promise.resolve(null));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(unauthorized());
   });
 });
