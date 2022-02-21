f54b515b25026c289938b16cc080983cebc6df04
Welisson luca <welissonluca17@gmail.com>
2022-02-21 08:52:25 -0300

    feat: ensure login controller returns 200 if valid credentials are provided

--- src/presentations/controllers/login/login.spec.ts
+++ src/presentations/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable new-cap */
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, serverError, unauthorized } from '../../helpers/http';
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http';
 import { LoginController } from './login';
 import {
   IHttpRequest,
@@ -136,5 +136,13 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  it('Should return 400 if invalid credentials are provided', async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }));
   });
 });
