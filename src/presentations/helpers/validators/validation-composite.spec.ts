c4feb30caf8bc54cd3459cb7882515ac01cbed44
Welisson luca <welissonluca17@gmail.com>
2022-02-22 09:05:45 -0300

    refactor: move sut creation to a factory helper method

--- src/presentations/helpers/validators/validation-composite.spec.ts
+++ src/presentations/helpers/validators/validation-composite.spec.ts
@@ -2,23 +2,36 @@ import { MissingParamError } from '../../errors/missingParamError';
 import { IValidation } from './validation';
 import { ValidationComposite } from './validation-composite';
 
+interface ISutTypes {
+  sut: ValidationComposite;
+  validationStub: IValidation;
+}
 
+const makeValidation = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(input: any): Error {
+      return null;
+    }
+  }
 
+  return new ValidationStub();
+};
 
-
+const makeSut = (): ISutTypes => {
+  const validationStub = makeValidation();
+  const sut = new ValidationComposite([validationStub]);
+  return {
+    sut,
+    validationStub,
+  };
+};
 describe('ValidationComposite Validation', () => {
   it('should return an error if any validation fails', () => {
-    class ValidationStub implements IValidation {
-      validate(input: any): Error {
-        return new MissingParamError('field');
-      }
-    }
-
-    const validationStub = new ValidationStub();
-    const sut = new ValidationComposite([validationStub]);
-
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
-
     expect(error).toEqual(new MissingParamError('field'));
   });
 });
