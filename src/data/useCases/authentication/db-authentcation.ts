66db5b41e4aa77da843ba05d2fadba96c6ed33fe
Welisson luca <welissonluca17@gmail.com>
2022-02-26 22:37:04 -0300

    feat: ensure db authentication calls hash compare with correct values

--- src/data/useCases/authentication/db-authentcation.ts
+++ src/data/useCases/authentication/db-authentcation.ts
@@ -2,14 +2,25 @@ import {
   IAuthentication,
   IAuthenticationModel,
 } from '../../../domain/useCases/authentication';
+import { IHashCompare } from '../../protocols/cripthografy/hash-compare';
 import { ILoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 
 export class DbAuthentication implements IAuthentication {
   constructor(
-    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+    private readonly hashCompare: IHashCompare
   ) {}
   async auth(authentication: IAuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email);
-    return null;
+    const account = await this.loadAccountByEmailRepository.load(
+      authentication.email
+    );
+
+    if (!account) {
+      return null;
+    }
+
+    this.hashCompare.compare(authentication.password, account.password);
+
+    return account.password;
   }
 }
