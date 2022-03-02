5117f621734022a02223eb7efb9ae1ebf9c4e322
Welisson luca <welissonluca17@gmail.com>
2022-03-02 08:34:29 -0300

    feat: ensure bcrypt adapter return a token on signin success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,6 +2,11 @@ import jwt from 'jsonwebtoken';
 
 import { JwtAdapter } from './jwt-adapter';
 
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return new Promise((resolve) => resolve('any_token'));
+  },
+}));
 describe('JwtAdapter', () => {
   it('should call signin with correct values', async () => {
     const sut = new JwtAdapter('secret');
@@ -13,5 +18,12 @@ describe('JwtAdapter', () => {
       },
       'secret'
     );
+  });
+
+  it('should return a token on signin success', async () => {
+    const sut = new JwtAdapter('secret');
+    const accessToken = await sut.encrypt('any_id');
+
+    expect(accessToken).toBe('any_token');
   });
 });
