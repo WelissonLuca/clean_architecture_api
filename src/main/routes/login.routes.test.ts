1ea46030290aea0cfa2da6a53536ed3f2c6a5fac
Welisson luca <welissonluca17@gmail.com>
2022-03-04 00:19:50 -0300

    refactor: rename signup routes to login routes

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -16,17 +16,18 @@ describe('Signup Routes', () => {
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
-  test('should return an account on success', async () => {
-    const response = await request(app)
-      .post('/api/signup/')
-      .send({
-        name: 'John Doe',
-        email: 'johndoe@email.com',
-        password: '123456',
-        passwordConfirmation: '123456',
-      })
-      .expect(200);
 
-    console.log(response);
+  describe('POST / Signup', () => {
+    test('should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup/')
+        .send({
+          name: 'John Doe',
+          email: 'johndoe@email.com',
+          password: '123456',
+          passwordConfirmation: '123456',
+        })
+        .expect(200);
+    });
   });
 });
