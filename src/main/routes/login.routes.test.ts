9f284af12c40505582a8a1a9399dff6bc1280077
Welisson luca <welissonluca17@gmail.com>
2022-03-04 00:32:46 -0300

    test: add login routes

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -1,15 +1,18 @@
+import { hash } from 'bcrypt';
+import { Collection } from 'mongodb';
 import request from 'supertest';
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
+let accountCollection: Collection;
 describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
@@ -26,6 +29,25 @@ describe('Login Routes', () => {
           email: 'johndoe@email.com',
           password: '123456',
           passwordConfirmation: '123456',
+        })
+        .expect(200);
+    });
+  });
+
+  describe('POST / Login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('123456', 12);
+      await accountCollection.insertOne({
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        password,
+      });
+
+      await request(app)
+        .post('/api/login/')
+        .send({
+          email: 'johndoe@email.com',
+          password: '123456',
         })
         .expect(200);
     });
