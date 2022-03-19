17af0b40729f157f3364cd0967e150f0efa79f10
Welisson luca <welissonluca17@gmail.com>
2022-03-18 23:24:44 -0300

    feat: ensure auth middleware calls load account by token with correct values

--- src/presentations/middlewares/auth-middleware.ts
+++ src/presentations/middlewares/auth-middleware.ts
@@ -1,9 +1,15 @@
+import { ILoadAccountByToken } from '../../domain/useCases/load-account-by-token';
 import { AccessDenied } from '../errors/access-denied-error';
 import { forbidden } from '../helpers/http/http';
 import { IMiddleware, IHttpRequest, IHttpResponse } from '../protocols';
 
 export class AuthMiddleware implements IMiddleware {
+  constructor(private readonly loadAccountByToken: ILoadAccountByToken) {}
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const accessToken = httpRequest.headers?.['x-access-token'];
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken);
+    }
     const error = forbidden(new AccessDenied());
     return new Promise((resolve) => resolve(error));
   }
--- src/presentations/protocols/http.ts
+++ src/presentations/protocols/http.ts
@@ -5,5 +5,5 @@ export interface IHttpResponse {
 
 export interface IHttpRequest {
   body?: any;
-  headers?: ANY;
+  headers?: any;
 }
