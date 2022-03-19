28b42a6c22c26d4868f789039fa6948a54f711f5
Welisson luca <welissonluca17@gmail.com>
2022-03-18 21:24:25 -0300

    test: ensure add a survey on sucess

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,43 @@
+import { Collection } from 'mongodb';
+
+import { MongoHelper } from '../helpers/mongo-helper';
+import { SurveyMongoRepository } from './survey-mongo-repository';
+
+let surveyCollection: Collection;
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository();
+};
+describe('Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+  test('Should add a survey on success', async () => {
+    const sut = makeSut();
+
+    await sut.add({
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer',
+        },
+        {
+          answer: 'any_answer',
+        },
+      ],
+      question: 'any_question',
+    });
+
+    const survey = await surveyCollection.findOne({ question: 'any_question' });
+    expect(survey).toBeTruthy();
+    expect(survey.question).toBe('any_question');
+  });
+});
