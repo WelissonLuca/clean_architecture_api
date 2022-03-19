5a9a62e5b303950132cd67829a8895b29d3a4683
Welisson luca <welissonluca17@gmail.com>
2022-03-18 23:25:03 -0300

    test: ensure auth middleware calls load account by token with correct values

--- src/presentations/middlewares/auth-middleware.spec.ts
+++ src/presentations/middlewares/auth-middleware.spec.ts
@@ -1,16 +1,34 @@
+import { IAccountModel } from '../../domain/models/account';
+import { ILoadAccountByToken } from '../../domain/useCases/load-account-by-token';
 import { AccessDenied } from '../errors/access-denied-error';
 import { forbidden } from '../helpers/http/http';
 import { IHttpRequest } from '../protocols/http';
 import { AuthMiddleware } from './auth-middleware';
 
+const makeFakeAccount = (): IAccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@email.com',
+  password: 'valid_password',
+});
+
+class LoadAccountByTokenStub implements ILoadAccountByToken {
+  async load(accessToken: string, role?: string): Promise<IAccountModel> {
+    return new Promise((resolve) => resolve(makeFakeAccount()));
+  }
+}
+
 interface ISutTypes {
   sut: AuthMiddleware;
+  loadAccountByTokenStub: LoadAccountByTokenStub;
 }
 
 const makeSut = (): ISutTypes => {
-  const authMiddleware = new AuthMiddleware();
+  const loadAccountByTokenStub = new LoadAccountByTokenStub();
+  const authMiddleware = new AuthMiddleware(loadAccountByTokenStub);
   return {
     sut: authMiddleware,
+    loadAccountByTokenStub,
   };
 };
 describe('Auth Middleware', () => {
@@ -19,5 +37,17 @@ describe('Auth Middleware', () => {
     const httpResponse = await sut.handle({});
 
     expect(httpResponse).toEqual(forbidden(new AccessDenied()));
+  });
+
+  test('Should call load account by token with correct access token', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load');
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token',
+      },
+    });
+
+    expect(loadSpy).toHaveBeenCalledWith('any_token');
   });
 });
