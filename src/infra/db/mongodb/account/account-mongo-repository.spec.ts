0c5e65facaa6ece725f4f674e67b58eb0d48910e
Welisson luca <welissonluca17@gmail.com>
2022-03-20 00:48:11 -0300

    test: ensure account mongo repository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -107,5 +107,25 @@ describe('Mongo Repository', () => {
       expect(account.email).toBe('any_mail@mail.com');
       expect(account.password).toBe('any_password');
     });
+
+    test('Should return an account on loadByToken with role', async () => {
+      const sut = makeSut();
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'admin',
+      });
+
+      const account = await sut.loadByToken('any_token', 'admin');
+
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_mail@mail.com');
+      expect(account.password).toBe('any_password');
+    });
   });
 });
