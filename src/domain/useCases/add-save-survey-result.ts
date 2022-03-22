0af45fdfe84f1a4b48caadfc38c87fedc2427a67
Welisson luca <welissonluca17@gmail.com>
2022-03-22 16:08:59 -0300

    feat: add save survey result use case

--- src/domain/useCases/add-save-survey-result.ts
+++ src/domain/useCases/add-save-survey-result.ts
@@ -2,6 +2,6 @@ import { SurveyResultModel } from '@domain/models/survey-result';
 
 export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>;
 
-export interface ILoadSurveyById {
+export interface IAddSaveSurveyResult {
   saveSurveyResult(data: SaveSurveyResultModel): Promise<SurveyResultModel>;
 }
