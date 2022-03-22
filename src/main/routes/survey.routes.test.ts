f35d916bf0d0c7a0a5bbfa3052a0a8ff8cfc9f0c
Welisson luca <welissonluca17@gmail.com>
2022-03-21 23:59:04 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey.routes.test.ts
+++ src/main/routes/survey.routes.test.ts
@@ -75,4 +75,58 @@ describe('Survey Routes', () => {
       await request(app).get('/api/surveys').expect(403);
     });
   });
+
+  test('should return 200 on load surveys with valid token', async () => {
+    await surveyCollection.insertMany([
+      {
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+          {
+            answer: 'any_answer',
+          },
+        ],
+        date: new Date(),
+      },
+      {
+        question: 'other_question',
+        answers: [
+          {
+            image: 'other_image',
+            answer: 'other_answer',
+          },
+          {
+            answer: 'other_answer',
+          },
+        ],
+        date: new Date(),
+      },
+    ]);
+
+    const res = await accountCollection.insertOne({
+      name: 'John Doe',
+      email: 'valid@mail.com',
+      password: '123456',
+    });
+
+    const id = res.ops[0]._id;
+    const accessToken = sign({ id }, env.jwtSecret);
+    await accountCollection.updateOne(
+      { _id: id },
+      {
+        $set: {
+          accessToken,
+        },
+      }
+    );
+
+    await request(app)
+      .get('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send()
+      .expect(200);
+  });
 });
