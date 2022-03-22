dcf5c383ab95b2377b9c3f643f8ab83c9e2248b0
Welisson luca <welissonluca17@gmail.com>
2022-03-21 23:36:54 -0300

    test: ensure db surveys throws if load surveys repository throws

--- src/data/useCases/db-load-surveys/db-load-surveys.spec.ts
+++ src/data/useCases/db-load-surveys/db-load-surveys.spec.ts
@@ -67,4 +67,17 @@ describe('DbLoadSurveys UseCase', () => {
 
     expect(surveys).toEqual(makeFakeSurveys());
   });
+
+  test('Should throw if load survey repository throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadSurveysRepositoryStub, 'loadAll')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const promise = sut.load();
+
+    expect(promise).rejects.toThrow();
+  });
 });
