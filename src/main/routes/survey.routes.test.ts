483cafb81aafc6095c5856b5b9630d479e47f06d
Welisson luca <welissonluca17@gmail.com>
2022-03-21 23:54:00 -0300

    test: ensure load surveys route return 403 if no access token is provided

--- src/main/routes/survey.routes.test.ts
+++ src/main/routes/survey.routes.test.ts
@@ -69,4 +69,10 @@ describe('Survey Routes', () => {
         .expect(204);
     });
   });
+
+  describe('GET /surveys', () => {
+    test('should return 403 on load surveys without accessToken', async () => {
+      await request(app).get('/api/surveys').expect(403);
+    });
+  });
 });
