a7994dfce7fb795fdb66c78d562b584eba39f248
Welisson luca <welissonluca17@gmail.com>
2022-03-22 16:08:18 -0300

    feat: add load survey by id

---
+++ src/domain/useCases/add-save-survey-result.ts
@@ -0,0 +1,7 @@
+import { SurveyResultModel } from '@domain/models/survey-result';
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>;
+
+export interface ILoadSurveyById {
+  saveSurveyResult(data: SaveSurveyResultModel): Promise<SurveyResultModel>;
+}
