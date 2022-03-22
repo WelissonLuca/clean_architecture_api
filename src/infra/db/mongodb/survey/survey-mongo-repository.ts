c167325d3ecdddf1f9273daf557f0b17ff478cbc
Welisson luca <welissonluca17@gmail.com>
2022-03-21 23:46:03 -0300

    feat: ensure survey mongo repository  load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,20 @@
 import { IAddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository';
+import { ILoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository';
+import { ISurveyModel } from '../../../../domain/models/survey';
 import { IAddSurveyModel } from '../../../../domain/useCases/addSurvey';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements IAddSurveyRepository {
+export class SurveyMongoRepository
+  implements IAddSurveyRepository, ILoadSurveysRepository
+{
   async add(surveyData: IAddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<ISurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const surveys = await surveyCollection.find().toArray();
+    return surveys;
   }
 }
