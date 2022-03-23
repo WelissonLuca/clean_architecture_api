04b355090c44356d4060b411c453a76b7711d170
Welisson luca <welissonluca17@gmail.com>
2022-03-23 01:08:26 -0300

    feat: ensure survey mongo repository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -2,11 +2,12 @@ import { IAddSurveyRepository } from '@data/protocols/db/survey/add-survey-repos
 import { ILoadSurveysRepository } from '@data/protocols/db/survey/load-survey-repository';
 import { SurveyModel } from '@domain/models/survey';
 import { AddSurveyModel } from '@domain/useCases/addSurvey';
+import { ILoadSurveyById } from '@domain/useCases/load-survey-by-id';
 
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class SurveyMongoRepository
-  implements IAddSurveyRepository, ILoadSurveysRepository
+  implements IAddSurveyRepository, ILoadSurveysRepository, ILoadSurveyById
 {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
@@ -17,5 +18,11 @@ export class SurveyMongoRepository
     const surveyCollection = await MongoHelper.getCollection('surveys');
     const surveys = await surveyCollection.find().toArray();
     return surveys;
+  }
+
+  async loadById(id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const survey = await surveyCollection.findOne({ _id: id });
+    return survey;
   }
 }
