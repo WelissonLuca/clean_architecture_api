9f03a63d8c80295dea9853294fdda31031d7df2d
Welisson luca <welissonluca17@gmail.com>
2022-03-23 01:08:38 -0300

    test: ensure survey mongo repository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -93,4 +93,27 @@ describe('Mongo Repository', () => {
       expect(surveys.length).toBe(0);
     });
   });
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+          {
+            answer: 'any_answer',
+          },
+        ],
+        date: new Date(),
+      });
+
+      const sut = makeSut();
+      const surveys = await sut.loadById(res.ops[0]._id);
+
+      expect(surveys).toBeTruthy();
+    });
+  });
 });
